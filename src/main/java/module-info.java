a3d29da35cd142e464ff6fcd95ddd582fce64848
JLGomezEncinar <240092@iessanalberto.com>
2025-03-17 13:51:33 +0100

    Commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,12 @@
 module com.iessanalberto.dam1.tallerespirineos {
     requires javafx.controls;
     requires javafx.fxml;
+    //requires javafx.base;
 
 
 
     opens com.iessanalberto.dam1.tallerespirineos to javafx.fxml;
+    opens com.iessanalberto.dam1.tallerespirineos.models to javafx.base;
+
     exports com.iessanalberto.dam1.tallerespirineos;
 }
